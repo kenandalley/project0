32a88f6d8af4448920524430c27587787bebe8fa
Kenan Dalley <kdalley@inbox.com>
2019-05-16 21:21:10 -0500

    refactor spaces

--- hash_table.c
+++ hash_table.c
@@ -4,12 +4,10 @@
 
 static int ARY_MULTIPLIER = 2;
 
+
 // Initialize the components of a hashtable.
-
 // The size parameter is the expected number of elements to be inserted.
-
 // This method returns an error code, 0 for success and -1 otherwise (e.g., if the parameter passed to the method is not null, if malloc fails, etc).
-
 int allocate(hashtable** ht, int size) {
     if (size < 1) {
         return -1;
@@ -33,21 +31,12 @@ int allocate(hashtable** ht, int size) {
     *ht = ht_temp;
 
     return 0;
-
 }
 
+
 // This method inserts a key-value pair into the hash table.
-
 // It returns an error code, 0 for success and -1 otherwise (e.g., if malloc is called and fails).
-
 int put(hashtable* ht, keyType key, valType value) {
-
-//    (void) ht;
-
-//    (void) key;
-
-//    (void) value;
-
     if (!ht) {
         return -1;
     }
@@ -76,26 +65,17 @@ int put(hashtable* ht, keyType key, valType value) {
     }
 
     return 0;
-
 }
 
 
 // This method retrieves entries with a matching key and stores the corresponding values in the
-
 // values array. The size of the values array is given by the parameter
-
 // num_values. If there are more matching entries than num_values, they are not
-
 // stored in the values array to avoid a buffer overflow. The function returns
-
 // the number of matching entries using the num_results pointer. If the value of num_results is greater than
-
 // num_values, the caller can invoke this function again (with a larger buffer)
-
 // to get values that it missed during the first call.
-
 // This method returns an error code, 0 for success and -1 otherwise (e.g., if the hashtable is not allocated).
-
 int get(hashtable* ht, keyType key, valType *values, int num_values, int* num_results) {
 
     (void) ht;
@@ -112,16 +92,12 @@ int get(hashtable* ht, keyType key, valType *values, int num_values, int* num_re
         return -1;
     }
 
-
-
     return 0;
-
 }
 
+
 // This method erases all key-value pairs with a given key from the hash table.
-
 // It returns an error code, 0 for success and -1 otherwise (e.g., if the hashtable is not allocated).
-
 int erase(hashtable* ht, keyType key) {
 
     (void) ht;
@@ -129,13 +105,11 @@ int erase(hashtable* ht, keyType key) {
     (void) key;
 
     return 0;
-
 }
 
+
 // This method frees all memory occupied by the hash table.
-
 // It returns an error code, 0 for success and -1 otherwise.
-
 int deallocate(hashtable* ht) {
     if (!ht) {
         return -1;
